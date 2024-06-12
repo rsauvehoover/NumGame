69d3c2564de7c30449b16afb7efeb72660076353
= <sauvehoover@gmail.com>
2024-06-12 15:30:37 -0700

    feat: some store boilerplate

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -1,5 +1,20 @@
 import { defineStore } from "pinia";
 
+// Possibly switch this out to somethign eldse? Set of pre-defined colour themes?
+export interface ColourTheme {
+  background: string;
+  text: string;
+}
+
+export interface Settings {
+  min: number;
+  max: number;
+  language: string;
+  // font? Dyslexic font maybe?
+  // colourTheme
+  // timer:
+}
+
 export const useSettingsStore = defineStore({
   id: "settings",
   state: () => ({})
