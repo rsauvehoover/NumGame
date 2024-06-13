0e36ba22d71e2006a5b0575105acb32edaeb159c
= <sauvehoover@gmail.com>
2024-06-12 18:37:12 -0700

    feat: update settings store getters/setters
    settings store now has setters/getters etc. and uses localstorage for persistance

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -1,21 +1,90 @@
 import { defineStore } from "pinia";
 
-// Possibly switch this out to somethign eldse? Set of pre-defined colour themes?
-export interface ColourTheme {
-  background: string;
-  text: string;
+const SETTINGS_LOCAL_STORAGE_KEY = "settings";
+
+export interface Range {
+  min: number;
+  max: number;
+}
+
+export enum Mode {
+  dark,
+  light
+}
+
+export enum Language {
+  en,
+  fr
 }
 
 export interface Settings {
-  min: number;
-  max: number;
-  language: string;
-  // font? Dyslexic font maybe?
-  // colourTheme
-  // timer:
+  range: Range;
+  language: Language;
+  mode: Mode;
+  timer: boolean;
+  dyslexicFont: boolean;
 }
+
+const getDefaultSettings = (): Settings => ({
+  range: {
+    min: 5,
+    max: 10
+  },
+  timer: false,
+  language: Language.en,
+  mode: Mode.dark,
+  dyslexicFont: false
+});
+
+const getSettings = () => {
+  const settings = localStorage.getItem(SETTINGS_LOCAL_STORAGE_KEY);
+
+  return settings ? JSON.parse(settings) : getDefaultSettings();
+};
 
 export const useSettingsStore = defineStore({
   id: "settings",
-  state: () => ({})
+  state: () => ({ settings: getSettings() }),
+  actions: {
+    updateSettings(partialSettings: Partial<Settings>) {
+      this.settings = {
+        ...this.settings,
+        ...partialSettings
+      };
+
+      localStorage.setItem(SETTINGS_LOCAL_STORAGE_KEY, JSON.stringify(this.settings));
+    },
+    setRange(range: Range) {
+      this.updateSettings({ range });
+    },
+    setMode(mode: Mode) {
+      this.updateSettings({ mode });
+    },
+    setLangauge(language: Language) {
+      this.updateSettings({ language });
+    },
+    toggleTimer() {
+      this.updateSettings({ timer: !this.settings.timer });
+    },
+    toggleDyslexicFont() {
+      this.updateSettings({ dyslexicFont: !this.settings.dyslexicFont });
+    }
+  },
+  getters: {
+    range(): Range {
+      return this.settings.range;
+    },
+    mode(): Mode {
+      return this.settings.mode;
+    },
+    language(): Language {
+      return this.settings.language;
+    },
+    timerActive(): boolean {
+      return this.settings.timer;
+    },
+    dyslexicFontActive(): boolean {
+      return this.settings.dyslexicFont;
+    }
+  }
 });
